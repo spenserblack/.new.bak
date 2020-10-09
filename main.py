36053453d4f9562e9254a8cd445adc95def938bf
Spenser Black <spenserblack01@gmail.com>
2020-10-09 08:36:44 -0400

    Add CLI args

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
-print("Hello, World!")
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument(
+    "directory", metavar="DIR", help="the directory to search for files"
+)
+parser.add_argument(
+    "--new",
+    metavar="EXTENSION",
+    default="new",
+    help="The placeholder extension of the new files",
+)
+parser.add_argument(
+    "--bak",
+    "--old",
+    metavar="EXTENSION",
+    default="bak",
+    help="The placeholder extension of the old files",
+)
+
+args = parser.parse_args()
+
+print("directory:", args.directory)
+print("new:", args.new)
+print("bak:", args.bak)
